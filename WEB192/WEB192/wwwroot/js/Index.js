7010f7497d940c8e248eea7537cad61e13c7a994
Eric Zuniga <egz11912@gmail.com>
2021-04-16 19:38:31 -0600

    Assignment Module 1

---
+++ WEB192/WEB192/wwwroot/js/Index.js
@@ -0,0 +1,27 @@
+﻿var divContainer = $("#MainContainer");
+
+$("<span></span").text("Web192").css("margin-right","10px").appendTo(divContainer);
+$("<button></button").text("Log In").appendTo(divContainer);
+$("<button></button").text("Sign Up").appendTo(divContainer);
+
+$("<br>").appendTo(divContainer);
+$("<br>").appendTo(divContainer);
+
+$("<span></span").text("Image 1").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span").text("Image 2").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span").text("Image 3").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span").text("Image 4").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span").text("Image 5").css("margin-right", "10px").appendTo(divContainer);
+
+$("<br>").appendTo(divContainer);
+$("<br>").appendTo(divContainer);
+ 
+$("<span></span").text("About").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span").text("Blog").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span").text("Jobs").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span").text("Help").css("margin-right", "10px").appendTo(divContainer);
+ 
+$("<br>").appendTo(divContainer);
+$("<span></span").text("English").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span").text("2021").css("margin-right", "10px").appendTo(divContainer);
+$("<span></span").text("WEB192").css("margin-right", "10px").appendTo(divContainer);
